bc4183b5f34ebc245dbccde305b5e773c59e4ed5
Hugo Daniel <hugodaniel.icam@gmail.com>
2019-09-24 20:05:53 +0200

    Fix owner grade nil error

--- app/helpers/star_rating_helper.rb
+++ app/helpers/star_rating_helper.rb
@@ -1,5 +1,9 @@
 module StarRatingHelper
   def display_stars(owner_grade)
-    icon('fas', 'star', class: "yellow-star") * owner_grade + icon('far', 'star', class: "yellow-star") * (5 - owner_grade)
+    if owner_grade
+      icon('fas', 'star', class: "yellow-star") * owner_grade + icon('far', 'star', class: "yellow-star") * (5 - owner_grade)
+    else
+      icon('far', 'star', class: "yellow-star") * 5
+    end
   end
 end
